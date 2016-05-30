6fd6b6f37a72af2b391189d9f536047ac98306f6
Alex <amurphy345@gmail.com>
2016-05-29 23:05:58 -0600

    github callback 2

--- server.js
+++ server.js
@@ -182,7 +182,7 @@ passport.use(new GitHubStrategy({
         clientID: GITHUB_CLIENT_ID,
         clientSecret: GITHUB_CLIENT_SECRET,
         // callbackURL: "http://127.0.0.1:3000/auth/github/callback"
-        callbackURL: "commitmap.herokuapp.com/auth/github/callback"
+        callbackURL: "http://commitmap.herokuapp.com/auth/github/callback"
     },
     function(accessToken, refreshToken, profile, done) {
         // asynchronous verification, for effect...
