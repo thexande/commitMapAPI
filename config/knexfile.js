1966eeea9fa08c9eb1ede05cb642d176f57f4285
Alex <amurphy345@gmail.com>
2016-05-29 22:36:56 -0600

    heroku pg 3

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,18 +1,19 @@
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: 'postgres://localhost/commitmap',
-      migrations: {
-        'directory': __dirname+"/migrations",
-        tableName: "users"
-      }
+    development: {
+        client: 'pg',
+        connection: 'postgres://localhost/commitmap',
+        migrations: {
+            'directory': __dirname + "/migrations",
+            tableName: "users"
+        }
     },
     heroku: {
-      client: 'pg',
-      connection: HEROKU_POSTGRESQL_COLOR_URL,
-      ssl: true,
-      migrations: {
-        'directory': __dirname+"/migrations",
-        tableName: "users"
+        client: 'pg',
+        connection: HEROKU_POSTGRESQL_COLOR_URL,
+        ssl: true,
+        migrations: {
+            'directory': __dirname + "/migrations",
+            tableName: "users"
+        }
     }
 }
