ebab09ee1d91b5f9b140c9f33337c76c149a9b13
Alex <amurphy345@gmail.com>
2016-05-31 15:09:37 -0600

    findOrCreate for heroku. 2

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var jsonwebtoken = require('jsonwebtoken');
 var jwtConfig = require('./config/jwtConfig')
 
 
-heroku
+// heroku
 var GITHUB_CLIENT_ID = "05380f6466ee28cc7524";
 var GITHUB_CLIENT_SECRET = "e2cd63e86c4b6090dbbace5a9282965591e37ba6";
 //
